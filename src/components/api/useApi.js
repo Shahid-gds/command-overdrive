e3ef43fbec65887a221e7e676a0a5b46c774aa47
Shahid-gds <shahid.gdsdev@gmail.com>
2024-11-28 06:06:39 +0500

    Updated

--- src/components/api/useApi.js
+++ src/components/api/useApi.js
@@ -1,8 +1,8 @@
 import { ref } from "vue";
 
 export function useApi() {
-  // const apiUrl = ref("https://sea-turtle-app-q8wy2.ondigitalocean.app/api/v1");
-  const apiUrl = ref("http://127.0.0.1:3000/api/v1");
+  const apiUrl = ref("https://sea-turtle-app-q8wy2.ondigitalocean.app/api/v1");
+  // const apiUrl = ref("http://127.0.0.1:3000/api/v1");
   
   const getApiUrl = () => apiUrl.value;
 
